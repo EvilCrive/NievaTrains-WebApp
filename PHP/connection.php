9354aa2efd97cc6ad6ac83f28bffcb0c350d15c6
MatBros <MatBros98@gmail.com>
2020-02-29 09:56:07 +0100

    funzione getQuery quasi bella

--- PHP/connection.php
+++ PHP/connection.php
@@ -10,8 +10,7 @@ class DBAccess
   public $connection=null;
 
 	/* Apre una connessione con il db con le variabili impostate precedentemente */
-  public function openConnectionlocal()
-  {
+  public function openConnectionlocal() {
     $this->connection = mysqli_connect(static::HOST_DB, static::user, static::pass, static::db_name);
     if (!$this->connection) {
       return false;
@@ -19,8 +18,7 @@ class DBAccess
 
     return true;
   }
-  public function openConnection()
-  {
+  public function openConnection() {
     $this->connection = mysqli_connect(static::HOST_DB, static::user, static::passDB, static::db_name);
     if (!$this->connection) {
       return false;
@@ -28,19 +26,29 @@ class DBAccess
 
     return true;
   }
-  private function getQuery($query)
-  {
-    $result = mysqli_query($this->connection, $query);
-    return mysqli_fetch_all($result, MYSQLI_ASSOC);
-  }
-
-
-
-  public function closeConnection()
-  {
+  public function closeConnection() {
     if ($this->connectionOpen)
       mysqli_close($this->connection);
   }
+  
+  public function getQuery($query) {
+	$result =$this->connection->query($query);
+    if(!$result) {
+        throw new Exception("errore query");
+    }
+    $array_file = [];
+    if($result->num_rows > 0) {
+        while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+            array_push($array_file,$row);
+        }
+        return $array_file;
+    }
+    else
+        return null;
+  }
+
+
+
 }
 
 public function getRicette($stringaCercata){
