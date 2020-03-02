7b1973954927b35132c16b903ba878108fa85769
EvilCrive <45993970+EvilCrive@users.noreply.github.com>
2020-03-02 17:03:30 +0100

    aggiornamento .

--- PHP/stampe.php
+++ PHP/stampe.php
@@ -12,7 +12,7 @@ function stampaRicerca($result) {
 		echo '<div class="responsive">'."\n";
 		echo '	<div class="gallery">'."\n";
 		echo '		<a target="_blank" href="'.$tmp["Nome_Immagine"].'.jpg">'."\n";
-		echo '		<img src="../Database/Ricette/'.$tmp["Nome_Immagine"].'jpg" alt="'.$tmp["Descrizione_Immagine"].'" width="600" height="400">'."\n";
+		echo '		<img src="../Database/Ricette/'.$tmp["Nome_Immagine"].'.jpg" alt="'.$tmp["Descrizione_Immagine"].'" width="600" height="400">'."\n";
 		echo '		</a>'."\n";
 		echo '	<div class="desc">'.$tmp["Nome"].'</div>'."\n";
 		echo '	</div>'."\n";
