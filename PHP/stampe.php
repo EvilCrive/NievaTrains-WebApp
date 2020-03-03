231e4c8524ed9cb2b3a773815e446ce2fb667e58
MatBros <MatBros98@gmail.com>
2020-03-03 15:40:33 +0100

    e anche l'utente a grandi linee è fatto

--- PHP/stampe.php
+++ PHP/stampe.php
@@ -7,14 +7,12 @@ function stampaRicerca($result) {
 	echo '<div class="rowconsigliate clear">'."\n";
 	$contatore=1;
 	for ($i=0; $i<$nrisultati; $i++){
-
-		$tmp=$result[$i];
 		echo '<div class="responsive">'."\n";
 		echo '	<div class="gallery">'."\n";
-		echo '		<a target="_blank" href="'.$tmp["Nome_Immagine"].'.jpg">'."\n";
-		echo '		<img src="../Database/Ricette/'.$tmp["Nome_Immagine"].'.jpg" alt="'.$tmp["Descrizione_Immagine"].'" width="600" height="400">'."\n";
+		echo '		<a target="_blank" href="'.$result[$i]["Nome_Immagine"].'.jpg">'."\n";
+		echo '		<img src="../Database/Ricette/'.$result[$i]["Nome_Immagine"].'.jpg" alt="'.$result[$i]["Descrizione_Immagine"].'" width="600" height="400">'."\n";
 		echo '		</a>'."\n";
-		echo '	<div class="desc">'.$tmp["Nome"].'</div>'."\n";
+		echo '	<div class="desc">'.$result[$i]["Nome"].'</div>'."\n";
 		echo '	</div>'."\n";
 		echo '</div>'."\n";
 		
@@ -29,49 +27,53 @@ function stampaRicerca($result) {
 	
 function stampaVoto($Connessione, $ID) {
 	$result=$Connessione->getQuery("SELECT Voto FROM Ricetta WHERE Id_Ricetta=$ID;");
-	$tmp=$result[0];
-	echo $tmp["Voto"];
+	echo $result[0]["Voto"];
 }
 function stampaPreferiti($Connessione, $ID) {
 	$result=$Connessione->getQuery("SELECT count(*) FROM Preferiti WHERE Id_Ricetta=$ID;");
-	$tmp=$result[0];
-	echo $tmp["count(*)"];
+	echo $result[0]["count(*)"];
 }
-function stampaImmagine() {}
 function stampaInformazioni($Connessione, $ID) {
 	$result=$Connessione->getQuery("SELECT Calorie, Dose, Costo, Difficoltà, Tempo_Preparazione
 		FROM Ricetta
 		WHERE Id_Ricetta=$ID;");
-		$tmp=$result[0];
-		echo '<li><i>€ Difficoltà : '.$tmp["Difficoltà"].'</i></li>';
-		echo '<li><i>€ Preparazione : '.$tmp["Tempo_Preparazione"].' minuti</i></li>';
-		echo '<li><i>€ Dosi per : '.$tmp["Dose"].' persone</i></li>';
-		echo '<li><i>€ Costo : '.$tmp["Costo"].'</i></li>';
-		echo '<li><i>€ Calorie : '.$tmp["Calorie"].'</i></li>';
+		echo '<li><i>€ Difficoltà : '.$result[0]["Difficoltà"].'</i></li>'."\n";
+		echo '<li><i>€ Preparazione : '.$result[0]["Tempo_Preparazione"].' minuti</i></li>'."\n";
+		echo '<li><i>€ Dosi per : '.$result[0]["Dose"].' persone</i></li>'."\n";
+		echo '<li><i>€ Costo : '.$result[0]["Costo"].'</i></li>'."\n";
+		echo '<li><i>€ Calorie : '.$result[0]["Calorie"].'</i></li>'."\n";
 }
 function stampaIngredienti($Connessione, $ID) { //dovrebbe essere una lista
 	$result=$Connessione->getQuery("SELECT Ingredienti FROM Ricetta WHERE Id_Ricetta=$ID;");
-	$tmp=$result[0];
-	echo $tmp["Ingredienti"];
+	echo $result[0]["Ingredienti"];
 }
 function stampaPasso($Connessione, $ID){
 	$result=$Connessione->getQuery("SELECT Passo_Passo FROM Ricetta WHERE Id_Ricetta=$ID;");
-	$tmp=$result[0];
-	echo $tmp["Passo_Passo"];
+	echo $result[0]["Passo_Passo"];
 }
 function stampaCommenti($Connessione, $ID) {
 	$result=$Connessione->getQuery("SELECT U.Nome, U.Cognome, U.Nome_Immagine, C.Testo, C.Data, C.Numero_Like FROM Commento as C JOIN Utente AS U ON C.Id_Utente=U.Id_Utente WHERE C.Id_Ricetta=$ID;");
 	$nrisultati=sizeof($result);
 	for ($i=0; $i<$nrisultati; $i++){
-	$tmp=$result[$i];
-		echo '<li class="comment">';
-        echo '	<div class="info">';
-        echo '  	<a href="#">'.$tmp["Nome"].' '.$tmp["Cognome"].'</a>';
-        echo '  </div>';
-		echo '	<p>'.$tmp["Testo"].'</p>';
-		echo '	</li>';
+		echo '<li class="comment">'."\n";
+        echo '	<div class="info">'."\n";
+        echo '  	<a href="#">'.$result[$i]["Nome"].' '.$result[$i]["Cognome"].'</a>'."\n";
+        echo '  </div>'."\n";
+		echo '	<p>'.$result[$i]["Testo"].'</p>'."\n";
+		echo '	</li>'."\n";
 	
 	}
+}
+function stampaImmagine($Connessione, $ID){
 	
+}
+function stampaFollowers($Connessione, $ID) {
+	$result=$Connessione->getQuery("SELECT U.Username
+		FROM Utente AS U JOIN Follow AS F ON U.Id_Utente=F.Id_Utente2
+		WHERE F.Id_Utente1=$ID;");
+	$nrisultati=sizeof($result);
+	for($i=0; $i<$nrisultati; $i++) { //andrebbe messo un limite
+	echo '<button class="button2">'.$result[$i]["Username"].'</button>'."\n";
+	}
 }
 ?>
