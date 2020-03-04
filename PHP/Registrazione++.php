2f76677b53b970e91871b757427dd29049f4927c
EvilCrive <45993970+EvilCrive@users.noreply.github.com>
2020-03-04 20:30:26 +0100

    work in progress v2

--- PHP/Registrazione++.php
+++ PHP/Registrazione++.php
@@ -1,7 +1,7 @@
 <?php
 require_once "connection.php";
 $ConnessioneAttiva = new DBAccess();
-
+$var=$ConnessioneAttiva->openConnectionlocal();
 $username=$_POST['Username'];
 $password=$_POST['Password'];
 $nome=$_POST['Nome'];
@@ -11,13 +11,21 @@ echo $username," ", $password, " ", $nome, " ", $cognome, " ", $email;
 	//errori
 	if(!empty($username) || !empty($password) || !empty($nome) || !empty($cognome) 
 		|| !empty($email)){
-		$var=$ConnessioneAttiva->openConnectionlocal();
+		
 		if($var)
 		{
 			
-			$query = "INSERT INTO utente VALUES(NULL,'$nome','$cognome','$username','$email','$password');";
-			echo $query;
-			$ConnessioneAttiva->getQuery($query);
+			$query = "INSERT INTO utente (Nome,Cognome,Username,Mail,Password) VALUES('$nome','$cognome','$username','$email','$password');";
+			/*$query = "INSERT INTO Utente
+          VALUES( NULL,
+                  '$nome',
+                  '$cognome',
+                  '$username',
+                  '$email',
+                  '$password');";*/
+
+			$r=$ConnessioneAttiva->getQuery($query);
+			echo r;
 			echo "Fantastico!","La tua iscrizione è avvenuta con successo. Tra qualche secondo ti mando alla Home.";
 			$_SESSION['id'] = $ConnessioneAttiva->getQuery("SELECT Id_Utente AS ID FROM Utente WHERE Mail='$email'")[0]['ID'];
 			$_SESSION['nome'] = $nome;
@@ -26,7 +34,6 @@ echo $username," ", $password, " ", $nome, " ", $cognome, " ", $email;
 			$_SESSION['username'] = $username;
 			$_SESSION['password'] = $password;
 			$_SESSION['login'] = true;
-			$ConnessioneAttiva->closeConnection();
   header( "refresh:4; url=../../index.php" ); 
 		}
 	}else{
