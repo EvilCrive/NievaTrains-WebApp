9c9a8ea1174e6ce0c625c90ece0a8f0375cafe5b
EvilCrive <45993970+EvilCrive@users.noreply.github.com>
2020-03-05 12:13:02 +0100

    work inn progress v3

--- PHP/Registrazione++.php
+++ PHP/Registrazione++.php
@@ -16,17 +16,11 @@ echo $username," ", $password, " ", $nome, " ", $cognome, " ", $email;
 		{
 			
 			$query = "INSERT INTO utente (Nome,Cognome,Username,Mail,Password) VALUES('$nome','$cognome','$username','$email','$password');";
-			/*$query = "INSERT INTO Utente
-          VALUES( NULL,
-                  '$nome',
-                  '$cognome',
-                  '$username',
-                  '$email',
-                  '$password');";*/
+			
 
-			$r=$ConnessioneAttiva->getQuery($query);
-			echo r;
+			$ConnessioneAttiva->getQuery($query);
 			echo "Fantastico!","La tua iscrizione è avvenuta con successo. Tra qualche secondo ti mando alla Home.";
+			session_start();
 			$_SESSION['id'] = $ConnessioneAttiva->getQuery("SELECT Id_Utente AS ID FROM Utente WHERE Mail='$email'")[0]['ID'];
 			$_SESSION['nome'] = $nome;
 			$_SESSION['cognome'] = $cognome;
---
+++ PHP/login.php
@@ -0,0 +1,38 @@
+<?php
+require_once "connection.php";
+$ConnessioneAttiva = new DBAccess();
+$var=$ConnessioneAttiva->openConnectionlocal();
+$password=$_POST['password'];
+$email=$_POST['email'];
+echo $password, " ", $email, " ";
+	//errori
+	if(!empty($password) || !empty($email)){
+		
+		if($var)
+		{
+			
+			$query = "SELECT* FROM utente WHERE Mail='$email' AND Password='$password';";
+			
+			$r=$ConnessioneAttiva->getQuery($query);
+			session_start();
+			$_SESSION['id'] = $ConnessioneAttiva->getQuery("SELECT Id_Utente AS ID FROM Utente WHERE Mail='$email'")[0]['ID'];
+			$_SESSION['nome'] = $ConnessioneAttiva->getQuery("SELECT Nome FROM Utente WHERE Mail='$email'");
+			$_SESSION['cognome'] = $ConnessioneAttiva->getQuery("SELECT Cognome FROM Utente WHERE Mail='$email'");
+			$_SESSION['email'] = $email;
+			$_SESSION['username'] = $ConnessioneAttiva->getQuery("SELECT Username FROM Utente WHERE Mail='$email'");
+			$_SESSION['password'] = $password;
+			$_SESSION['login'] = true;
+			
+			print_r($_SESSION['nome']);
+			print_r($_SESSION['cognome']);
+			
+  header( "refresh:4; url=indexx.php" ); 
+		}
+	}else{
+		echo "Completa i campi.";
+		die();
+	}
+	
+
+
+?>
