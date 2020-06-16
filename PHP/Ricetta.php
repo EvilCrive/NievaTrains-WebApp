3506eb3e6c7c4f66f0c7c7010f3633c0ff83205b
EvilCrive <crivealby@gmail.com>
2020-06-16 19:55:53 +0200

    about.php e ritocchi

--- PHP/Ricetta.php
+++ PHP/Ricetta.php
@@ -14,7 +14,7 @@ try{
 		$ricetta=$connessione->getQuery("SELECT * FROM ricetta WHERE Id_Ricetta=$ID;");
 		$prefe=$connessione->getQuery("SELECT count(*) FROM Preferiti WHERE Id_Ricetta=$ID;");
 	//getquery commenti(id)
-		$commenti=$connessione->getQuery("SELECT U.Id_Utente, U.Nome, U.Cognome, U.Nome_Immagine, C.Id_Ricetta, C.Id_Commento, C.Testo, C.Data, C.Numero_Like FROM commento as C JOIN Utente AS U ON C.Id_Utente=U.Id_Utente WHERE C.Id_Ricetta=$ID ORDER BY C.Data;");
+		$commenti=$connessione->getQuery("SELECT U.Id_Utente, U.Nome, U.Cognome, U.Nome_Immagine, C.Id_Ricetta, C.Id_Commento, C.Testo, C.Data FROM commento as C JOIN Utente AS U ON C.Id_Utente=U.Id_Utente WHERE C.Id_Ricetta=$ID ORDER BY C.Data;");
 	//getquery correlate(categoria)
 		$cat=$ricetta[0]['Macro_Categoria'];
 		$correlate=$connessione->getQuery("SELECT * FROM ricetta WHERE Macro_Categoria='$cat';");
