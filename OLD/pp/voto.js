ddec5cb8a4c7d42fbc526cadc15fa2542f5b3476
EvilCrive <crivealby@gmail.com>
2020-06-19 19:59:39 +0200

    worka le stelle

--- OLD/pp/voto.js
+++ OLD/pp/voto.js
@@ -1,9 +1,12 @@
-const stars = document.querySelector('.ratings').children;
+document.addEventListener("DOMContentLoaded", function(){
+  const stars = document.querySelector('.ratings').children;
 const ratingValue = document.querySelector('#rating-value');
 let index;
 
 for(let i=0; i<stars.length; i++){
-  stars[i].addEventListener("mouseover", function(){
+
+  stars[i].addEventListener("mouseover", 
+  function(){
     for(let j=0; j<stars.length; j++){
       stars[j].classList.remove("fa-star");
       stars[j].classList.add("fa-star-o");
@@ -13,6 +16,8 @@ for(let i=0; i<stars.length; i++){
       stars[j].classList.add("fa-star");
     }
   })
+
+
   stars[i].addEventListener("click", function(){
     ratingValue.value=i+1;
     index=i;
@@ -28,6 +33,45 @@ for(let i=0; i<stars.length; i++){
     }
   })
 }
+});
+
+
+/*
+const stars = document.querySelector('.ratings').children;
+const ratingValue = document.querySelector('#rating-value');
+let index;
+
+for(let i=0; i<stars.length; i++){
+
+  stars[i].addEventListener("mouseover", 
+  function(){
+    for(let j=0; j<stars.length; j++){
+      stars[j].classList.remove("fa-star");
+      stars[j].classList.add("fa-star-o");
+    }
+    for(let j=0; j<=i; j++){
+      stars[j].classList.remove("fa-star-o");
+      stars[j].classList.add("fa-star");
+    }
+  })
+
+
+  stars[i].addEventListener("click", function(){
+    ratingValue.value=i+1;
+    index=i;
+  })
+  stars[i].addEventListener("mouseout", function(){
+    for(let j=0; j<stars.length; j++){
+      stars[j].classList.remove("fa-star");
+      stars[j].classList.add("fa-star-o");
+    }
+    for(let j=0; j<=index; j++){
+      stars[j].classList.remove("fa-star-o");
+      stars[j].classList.add("fa-star");
+    }
+  })
+}
+*/
 
 /*
   function showRating(e){
