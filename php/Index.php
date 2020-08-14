d858eaef2251c70ecabc5148b4adc20b92b58797
MatBros <MatBros98@gmail.com>
2020-08-14 08:59:31 +0200

    struttura php

---
+++ php/Index.php
@@ -0,0 +1,17 @@
+<?php
+//importazione librerie
+
+//inizializzazione session
+
+//connessione al db
+
+//generazione variabili di sostituzione
+
+//importazione txt
+
+//sostituzione variabili di sostituzione
+
+//gestione eccezioni
+
+//chiusura connessione
+?>
