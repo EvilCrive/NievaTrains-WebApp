01f4621b068a497dca6aede15509a40a240a33e2
MatBros <MatBros98@gmail.com>
2020-09-07 08:29:52 +0200

    cose minori

--- php/utils/operations.php
+++ php/utils/operations.php
@@ -43,7 +43,7 @@ try{
             $testoCommento=$_POST['testoCommento'];
             $testoCommento=strip_tags($testoCommento);
             $errors="";
-            if (!preg_match('/^[a-z0-9]{3,500}$/i',$testoCommento))        $errors.="<li>Commento non valido</li>";
+            if (!preg_match('/^[a-z0-9 ]{3,500}$/i',$testoCommento))        $errors.="<li>Commento non valido</li>";
             else    addCommento($id,$treno,$testoCommento,$connessione);
             header("refresh:0 url=../Treno.php?Id_Treno=$treno#commentform"); 
         }
